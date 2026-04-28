4e6c7e823917b9539797f11ec0eea6103544e58e
YJ <eu9309@naver.com>
2026-04-28 10:20:28 +0900

    0428

---
+++ Concepts/02_db_sql/03_modifying_data.sql
@@ -0,0 +1,16 @@
+-- 공통
+SELECT * FROM articles;
+DROP TABLE articles;
+PRAGMA table_info('articles');
+
+
+-- 1. Insert data into table
+
+
+-- 2. Update data in table
+
+
+-- 3. Delete data from table
+
+
+-- 심화
